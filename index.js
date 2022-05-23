72f203cc8759ac5f4a08ec8825945d0bae9bc975
angelique-df <faye.angelique.d@gmail.com>
2022-05-23 23:07:22 +0000

    Update index.js
    Use `sqlite3` to create a database

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+const db = new sqlite3.Database(
+  path.resolve(__dirname, "./database.sqlite"),
+  (err) => {
+    if (err) {
+      console.log(err);
+    }
+  }
+);
+
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
