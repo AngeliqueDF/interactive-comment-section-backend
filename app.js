a9e153059b683b0564705a0ccbe49711bc712edf
angelique-df <faye.angelique.d@gmail.com>
2022-05-24 23:48:18 +0000

    Update app.js
    Use `helmet` to set security headers

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
 app.use(express.json());
+
+app.use(helmet);
+// TODO add sanitizer middleware
 module.exports = app;
