811130260570e60e2fbc62c91673964a0b756c05
angelique-df <faye.angelique.d@gmail.com>
2022-08-18 16:35:18 +0000

    Update `require` for `db`

--- utils/database/comments/addReply.js
+++ utils/database/comments/addReply.js
@@ -1,6 +1,8 @@
 const path = require("path");
-const { db } = require(path.resolve(__dirname, "./../connectTestDatabase"));
-
+const db = require(path.resolve(
+	__dirname,
+	"./../../../models/connectUtilsTestDatabase.js"
+))();
 const Comment = require(path.resolve(__dirname, "./../../../models/comment"));
 
 const statement = db.prepare(Comment.newComment());
