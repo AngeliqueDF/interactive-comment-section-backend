4b3a6e1110b8bf22d81a44e6c81874109d921bbf
angelique-df <faye.angelique.d@gmail.com>
2022-08-23 18:42:12 +0000

    Update `createUsersTable`
    - Update column names
    - Simplify syntax

--- models/user.js
+++ models/user.js
@@ -1,10 +1,8 @@
-const createUsersTable = () => {
-  return `CREATE TABLE users (
+const createUsersTable = () => `CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
-    png_profile_image TEXT,
-    webp_profile_image TEXT
+    png TEXT,
+    webp TEXT
   );`;
-};
 
 module.exports = { createUsersTable };
