e5eea60b76738fa835aa33ed122a2e7ae719e21b
angelique-df <faye.angelique.d@gmail.com>
2022-08-24 18:42:02 +0000

    Add `POST /api/comments` (WIP)
    - Prevents XSS attacks
    - Replaces missing optional values with `null`

---
+++ controllers/comments.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const { db } = require(path.resolve(
+	__dirname,
+	"./../models/connectDatabase"
+))();
+const Comment = require(path.resolve(__dirname, "./../models/comment"));
+
+const xss = require("xss");
+const validator = require("validator");
+const commentsRouter = require("express").Router();
+
+/**
+ * Add a new comment
+ */
+commentsRouter.post(
+	"/",
+	(req, res) => {
+		const newComment = {
+			user: Number(xss(req.body.user)),
+			content: xss(req.body.content),
+			createdAt: new Date(),
+			replyingToUser: Number(xss(req.body.replyingToUser)) || null,
+			replyingToComment: Number(xss(req.body.replyingToComment)) || null,
+		};
+		const statement = db.prepare(Comment.newComment());
+
+		statement.run(
+			[
+				newComment.user,
+				newComment.content,
+				new Date(),
+				newComment.score,
+				newComment.replyingToComment,
+				newComment.replyingToUser,
+			],
+			(err) => {
+				if (err) {
+					console.log(err);
+					return res
+						.status(500)
+						.json({ error: "There was an error. Please try again later." });
+				}
+				res.status(201).json({ ...newComment });
+			}
+		);
+	}
+);
+
+module.exports = commentsRouter;
