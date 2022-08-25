bc2777e8e64455d92ddbf0b56029d90111950cc2
angelique-df <faye.angelique.d@gmail.com>
2022-08-25 19:19:45 +0000

    Return a promise from `runPreparedStatement`
    - Resolves with the `id` of the last row affected by the query.
    - Rejects with an eventual `SQL` error.

--- models/Database.js
+++ models/Database.js
@@ -2,18 +2,14 @@ const path = require("path");
 const { db } = require(path.resolve(__dirname, "./connectDatabase.js"))();
 
 function runPreparedStatement(sqlQuery, parameters) {
-	const statement = db.prepare(sqlQuery);
-	statement.run(parameters, async function (err) {
-		if (err) {
-			console.log("database error", err);
-			return err;
-		}
-		console.log("no error", "this.lastID", this.lastID);
-		// console.log(this);
-		let result;
-		result = await this.lastID;
-		console.log("result", result);
-		return result;
+	return new Promise(function (resolve, reject) {
+		const statement = db.prepare(sqlQuery);
+		statement.run(parameters, function (err) {
+			if (err) {
+				reject(err);
+			}
+			resolve(this.lastID);
+		});
 	});
 
 	statement.finalize();
