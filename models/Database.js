f15f8dfd4f412b3fa6f2e0b38bf255150851d0dd
angelique-df <faye.angelique.d@gmail.com>
2022-09-05 14:52:52 +0000

    Update Database.js
    - Fix connecting to the wrong database in certain environments.
    
    - Change import of `connectDatabase` since its export has changed.
    
    Which database the `Database` module connects to depends on `process.env.NODE_ENV`'s value.

--- models/Database.js
+++ models/Database.js
@@ -1,5 +1,12 @@
+/**
+ * Prepares and runs database statements. Resolves or rejects depending on the success/failure of queries.
+ */
+
 const path = require("path");
-const { db } = require(path.resolve(__dirname, "./connectDatabase.js"))();
+const db = require(path.resolve(
+	__dirname,
+	"./connectDatabase.js"
+)).connectDatabase();
 
 function runPreparedStatement(sqlQuery, parameters) {
 	return new Promise(function (resolve, reject) {
@@ -9,15 +16,22 @@ function runPreparedStatement(sqlQuery, parameters) {
 				reject(err);
 			}
 			resolve(this.lastID);
+			statement.finalize();
 		});
 	});
-
-	statement.finalize();
 }
-function runQuery(sqlQuery) {
-	db.run(sqlQuery);
+function getAll(sqlQuery) {
+	return new Promise(function (resolve, reject) {
+		db.all(sqlQuery, function (err, rows) {
+			if (err) {
+				reject(err);
+			}
+			resolve(rows);
+		});
+	});
 }
 
 module.exports = {
 	addOne: runPreparedStatement,
+	getAll,
 };
