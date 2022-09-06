fea23825c6667275ae3cfc0886bbd75e175e230a
angelique-df <faye.angelique.d@gmail.com>
2022-09-06 11:54:42 +0000

    Update comments.controllers.js
    - Add `checkMissingContent`

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -6,3 +6,16 @@ const CommentModel = require(path.resolve(__dirname, "./../models/comment"));
 // Strips dangerous characters from data received from clients.
 const xss = require("xss");
 
+/**
+ * Checks the content property is defined in a request body.
+ */
+function checkMissingContent(req, res, next) {
+	if (!req.body.content) {
+		return res.status(400).json({ error: "Missing required field." });
+	}
+	next();
+}
+
+module.exports = {
+	checkMissingContent,
+};
