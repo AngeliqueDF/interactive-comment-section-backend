93cf22422c482062548773b2fd571c8d80fc01ea
angelique-df <faye.angelique.d@gmail.com>
2022-09-06 13:20:42 +0000

    Update app.js
    - Use `errorHandler` middleware.

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const helmet = require("helmet");
 
 const commentsRouter = require("./controllers/comments.routes");
 
+const middleware = require("./utils/middleware");
+
 app.use(helmet());
 app.use(express.json());
 
 app.use("/api/comments", commentsRouter);
+app.use(middleware.errorHandler);
 
 module.exports = app;
