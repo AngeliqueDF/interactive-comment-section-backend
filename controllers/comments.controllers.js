0f88bc683f9adc7d9b1d0794487071b8d63e20a9
angelique-df <faye.angelique.d@gmail.com>
2022-09-06 20:13:14 +0000

    Update comments.controllers.js

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -3,7 +3,7 @@ const path = require("path");
 // Modules interacting with the database
 const CommentModel = require(path.resolve(__dirname, "./../models/comment"));
 
-// Strips dangerous characters from data received from clients.
+// Strips dangerous characters from data sent by clients.
 const xss = require("xss");
 
 /**
