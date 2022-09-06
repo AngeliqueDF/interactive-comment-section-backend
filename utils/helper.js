45e22be12733e2892df0c1dd32a0a02bee18546d
angelique-df <faye.angelique.d@gmail.com>
2022-09-06 20:24:35 +0000

    Create helper.js
    - Add `findRootComment`

---
+++ utils/helper.js
@@ -0,0 +1,25 @@
+/**
+ * Uses recursion to find the root comment of a reply.
+ */
+const findRootComment = function (allComments, currentCommentID) {
+	// console.log(allComments, currentCommentID);
+	// Find a comment in the state by its id.
+	let currentComment = allComments.find(
+		(comment) => comment.id === currentCommentID
+	);
+
+	// If the current comment is a reply to another comment...
+	if (currentComment.replyingToComment != null) {
+		// ...store the other comment's id in currentComment.
+		currentComment = currentComment.replyingToComment;
+		// Then call the function again
+		return findRootComment(currentComment);
+	}
+
+	// Once we reach a comment that has null in replyingToComment, we have found the root comment. We can return its id.
+	return currentComment.id;
+};
+
+module.exports = {
+	findRootComment,
+};
