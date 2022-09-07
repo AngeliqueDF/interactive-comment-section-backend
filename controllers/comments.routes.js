b3bb7fd709008787f21f382cebf01aad93273a1a
angelique-df <faye.angelique.d@gmail.com>
2022-09-07 18:01:15 +0000

    Define new paths for separate controllers
    - Apply `setRootComment` middleware only for the `/newReply` path.

--- controllers/comments.routes.js
+++ controllers/comments.routes.js
@@ -6,7 +6,16 @@ const CommentController = require("./comments.controllers");
  * Add a new comment
  */
 CommentsRouter.post(
-	"/",
+	"/newComment",
+	CommentController.checkMissingContent,
+	CommentController.insertComment,
+	(req, res) => {
+		res.status(201).json(req.body.newComment);
+	}
+);
+
+CommentsRouter.post(
+	"/newReply",
 	CommentController.checkMissingContent,
 	CommentController.insertComment,
 	CommentController.setRootComment,
