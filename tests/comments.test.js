f84df952ad606dc22e427ddd7a88dc1ca3500b69
angelique-df <faye.angelique.d@gmail.com>
2022-09-09 18:38:46 +0000

    Change setup of `/api/comments`
    Isolate tests ARRANGE step
    - Remove sample data
    - Remove `beforeEach` hook

--- tests/comments.test.js
+++ tests/comments.test.js
@@ -16,43 +16,10 @@ const API_URL = "/api/comments";
 const spy = jest.spyOn(global, "Date");
 
 describe('GET "/api/comments"', () => {
-	// ARRANGE
-	const INITIAL_COMMENTS = [
-		{
-			user: 0,
-			content:
-				"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-			createdAt: new Date(),
-			score: 12,
-		},
-		{
-			user: 1,
-			content:
-				"Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-			createdAt: new Date(),
-			score: 5,
-		},
-		{
-			user: 2,
-			content:
-				"If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-			createdAt: new Date(),
-			score: 4,
-			replyingTo: "maxblagun",
-		},
-		{
-			user: 3,
-			content:
-				"I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-			createdAt: new Date(),
-			score: 2,
-			replyingTo: "ramsesmiron",
-		},
-	];
+	afterEach(() => {
+		db.run(`DELETE FROM comments;`);
+	});
 
-	beforeEach(() => {
-		// add
-		INITIAL_COMMENTS.forEach(async (comment) => {
 			try {
 				const addedComment = await Comment.insertOne([
 					comment.user,
@@ -66,12 +33,8 @@ describe('GET "/api/comments"', () => {
 				console.log(error);
 			}
 		});
-	});
 
-	afterEach(() => {
-		db.run(`DELETE FROM comments;`);
 	});
-	// End arrange
 
 	test("Returns all comments in the database", async () => {
 		const response = await api
