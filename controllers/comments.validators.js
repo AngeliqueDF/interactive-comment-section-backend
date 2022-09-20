26ce95fbfb97fcf78b9c8f7c89ce8a523f9aff6f
angelique-df <faye.angelique.d@gmail.com>
2022-09-20 17:32:47 +0000

    Validate `POST /api/comments/newReply` req.body

--- controllers/comments.validators.js
+++ controllers/comments.validators.js
@@ -28,4 +28,44 @@ CommentsValidator.post(
 	}
 );
 
+/**
+ * Validate request body when adding a new reply
+ */
+CommentsValidator.post(
+	"/newReply",
+	checkSchema({
+		["newComment.user"]: {
+			in: ["body"],
+			isInt: true,
+		},
+		["newComment.replyingToComment"]: {
+			in: ["body"],
+			isInt: true,
+		},
+		["newComment.replyingToUser"]: {
+			in: ["body"],
+			isInt: true,
+		},
+		"newComment.content": {
+			in: ["body"],
+			isString: true,
+			isLength: { options: { min: 1 } },
+		},
+		"newComment.replyingToAuthor": {
+			in: ["body"],
+			isString: true,
+			isLength: { options: { min: 1 } },
+		},
+	}),
+	(req, res, next) => {
+		const result = validationResult(req);
+		if (!result.isEmpty()) {
+			const error = new Error();
+			error.name = "ValidationError";
+			next(error);
+		}
+		next();
+	}
+);
+
 module.exports = CommentsValidator;
