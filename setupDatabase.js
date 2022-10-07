16cb4e8ccf58942a5c6874ea0df3f5852092d04e
angelique-df <faye.angelique.d@gmail.com>
2022-10-07 14:24:07 +0000

    Create setupDatabase.js
    Methods
    - `createDatabase` creates the database
    - `clearDatabase` removes all data from database. Although this method is available, it doesn't seem to work in test files.
    
    Tables aren't deleted anymore since this would cause errors when running `jest` tests.

---
+++ setupDatabase.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const db = require(path.resolve(
+	__dirname,
+	"./models/database.connection"
+)).connectDatabase();
+
+const Comment = require("./models/comments.model");
+const CommentsVotes = require("./models/comments.votes.model");
+const User = require("./models/user");
+
+function createDatabase() {
+	return new Promise(function (resolve, reject) {
+		db.serialize(() => {
+			db.run(User.CREATE_USERS_TABLE_QUERY, (err) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				}
+			});
+
+			db.run(Comment.CREATE_COMMENTS_TABLE_QUERY, (err) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				}
+			});
+
+			db.run(CommentsVotes.CREATE_COMMENTS_VOTES_TABLE_QUERY, (err) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				}
+			});
+			resolve();
+		});
+	});
+}
+
+function clearDatabase() {
+	db.serialize(() => {
+		db.run(`DELETE FROM comments;`);
+		db.run(`DELETE FROM comments_votes;`);
+		db.run(`DELETE FROM users;`);
+	});
+}
+
+module.exports = { createDatabase, clearDatabase };
