14078bfa2610903e54b8ef4679887d17fc4f02d3
angelique-df <faye.angelique.d@gmail.com>
2022-10-07 14:26:46 +0000

    Update index.js
    Use `setupDatabase` module.

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
 const path = require("path");
-const db = require(path.resolve(
-	__dirname,
-	"./models/database.connection"
-)).connectDatabase();
-
-const Comment = require("./models/comments.model");
-const CommentsVotes = require("./models/comments.votes.model");
-const User = require("./models/user");
+const setupDatabase = require(path.resolve(__dirname, "./setupDatabase"));
 
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 5000, async () => {
 	console.log(`Server running on port ${process.env.PORT || 5000}`);
 	console.log("Please wait while the database is created");
 
-	db.serialize(() => {
-		db.run(Comment.DROP_COMMENTS_TABLE_QUERY);
-		db.run(CommentsVotes.DROP_COMMENTS_VOTES_TABLE_QUERY);
-		db.run(User.DROP_USERS_TABLE_QUERY);
-
-		db.run(User.CREATE_USERS_TABLE_QUERY, (err) => {
-			if (err) {
-				console.log(err);
-			}
-			console.log("\x1b[34m", "Table users created");
-		});
-
-		db.run(Comment.CREATE_COMMENTS_TABLE_QUERY, (err) => {
-			if (err) {
-				console.log(err);
-			}
-			console.log("\x1b[34m", "Table comments created");
-		});
-
-		db.run(CommentsVotes.CREATE_COMMENTS_VOTES_TABLE_QUERY, (err) => {
-			if (err) {
-				console.log(err);
-			}
-			console.log("\x1b[34m", "Table comments_votes created");
-		});
-	});
+	await setupDatabase.createDatabase();
 });
+
+module.exports = server;
